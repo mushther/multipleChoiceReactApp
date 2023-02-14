087aaf8d7feb75b911e4c112a808b7a07d2e2dd3
mushther <mushther2121999@gmai.com>
2023-02-15 00:20:21 +0530

    add navbar code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
 
+import { Box, Button, ButtonGroup, Flex, Heading, Input, Spacer } from '@chakra-ui/react'
 import React from 'react'
+import { GoDiffAdded } from "react-icons/go";
+import { BsListUl } from "react-icons/bs";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <Flex m={0} top={0} w={'100%'} position={'fixed'} minWidth='max-content' bg={'#e5d2d2a9'} alignItems='center' gap='2' p='10px'>
+            <Box p='2'>
+                <Heading
+                    size='md'
+                    fontFamily={'4'}
+                    bgGradient='linear(to-l, #7928CA, #FF0080)'
+                    bgClip='text'
+                    fontSize='3xl'
+                    fontWeight='extrabold'
+                >Online Quiz App</Heading>
+            </Box>
+            <Spacer />
+            <Flex w='65%' alignItems={'center'}>
+                <Input border={'1px solid grey'} placeholder='Search ' />
+            </Flex>
+            <ButtonGroup gap='2'>
+                <Button p={4}
+                    color='white'
+                    fontWeight='bold'
+                    display={'flex'}
+                    gap='10px'
+                    alignItems={'center'}
+                    borderRadius='md'
+                    bgGradient='linear(to-r, teal.500, green.500)'
+                    _hover={{
+                        bgGradient: 'linear(to-r, red.500, yellow.500)',
+                    }}><BsListUl /> Start Quiz Test</Button>
+                <Button p={4}
+                    color='white'
+                    fontWeight='bold'
+                    borderRadius='md'
+                    bgGradient='linear(to-r, teal.500, green.500)'
+                    _hover={{
+                        bgGradient: 'linear(to-r, red.500, yellow.500)',
+                    }}><GoDiffAdded /></Button>
+            </ButtonGroup>
+        </Flex>
     )
 }
 
